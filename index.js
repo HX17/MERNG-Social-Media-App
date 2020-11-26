3f5513e3075e474c92f34f4c055537de5e8770a2
HX17 <u4983817@gmail.com>
2020-11-27 00:48:23 +1100

    complete backend

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config");
+const resolvers = require("./graphql/resolvers");
+const typeDefs = require("./graphql/typeDefs");
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub }),
+});
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
