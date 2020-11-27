167977837c67110519d38e5949aded111ffbfc69
HX17 <u4983817@gmail.com>
2020-11-28 00:23:36 +1100

    complete login and register page

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,6 +3,7 @@ const usersResolvers = require("./users");
 const commentsResolvers = require("./comments");
 
 module.exports = {
+  // query or mutation regarding post go through this fn below and add props
   Post: {
     likeCount: (parent) => parent.likes.length,
     commentCount: (parent) => parent.comments.length,
